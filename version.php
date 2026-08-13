7e8bf853b9c7b806c7e89282fe18b3772d52ff74
sambapete <pierre.mailhot@gmail.com>
2026-08-13 14:15:00 -0400

    version incrémentée de 1

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_testdeploy';
-$plugin->version   = 2026081300;
+$plugin->version   = 2026081301;
 $plugin->requires  = 2022041900; // Moodle 4.0
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '1.3.5';
