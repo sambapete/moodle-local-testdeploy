8fbc2c28f379da99a85acf239410131fbea365f3
sambapete <pierre.mailhot@gmail.com>
2026-08-13 14:51:38 -0400

    Nouvelle version et nouveau release

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_testdeploy';
-$plugin->version   = 2026081303;
+$plugin->version   = 2026081304;
 $plugin->requires  = 2022041900; // Moodle 4.0
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '1.3.7';
+$plugin->release   = '1.3.8';
