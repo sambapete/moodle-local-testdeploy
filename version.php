d0b122dc1e1ee930c7f63850f01fce0a52fb1886
sambapete <pierre.mailhot@gmail.com>
2026-08-13 14:04:55 -0400

    passe à la version 1.2.5 dans main

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ $plugin->component = 'local_testdeploy';
 $plugin->version   = 2026081300;
 $plugin->requires  = 2022041900; // Moodle 4.0
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '1.2.0';
+$plugin->release   = '1.2.5';
